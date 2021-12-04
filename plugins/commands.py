9f8c80911871ed8c97ebec70945eb9ec380683eb
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 10:16:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,11 +6,15 @@ else:
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-@Client.on_message(filters.command(["start"]) & filters.private)
-async def start(bot, update):
-    await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS
-    )
+@Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
+async def ping_handler(c: Client, m: "types.Message"):
+    if not m.from_user:
+        return await m.reply_text("I don't know about you sar :(")
+    await m.reply_text("Hi, I am Rename Bot!\n\n"
+                       "I can rename media without downloading it!\n"
+                       "Speed depends on your media DC.\n\n"
+                       "Just send me media and reply to it with /rename command.",
+                       reply_markup=types.InlineKeyboardMarkup([[
+                           types.InlineKeyboardButton("Show Settings",
+                                                      callback_data="showSettings")
+                       ]]))

60c9f259062c42205033953104876f71794279d9
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 19:23:11 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -34,13 +34,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["getlink"]))
 async def get_link(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
     logger.info(update.from_user)
     if update.reply_to_message is not None:
         reply_message = update.reply_to_message

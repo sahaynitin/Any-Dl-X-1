8160580e6f5a79ea3f8ee7cb35cd3ff69a081d0f
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 08:26:29 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -161,7 +161,7 @@ async def echo(bot, update):
         with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
             json.dump(response_json, outfile, ensure_ascii=False)
         # logger.info(response_json)
-        cb_data = Message.data
+        cb_data = Translation.FORMAT_SELECTION.format("")
         if media_type == 'audio':
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
                 "Audio", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",

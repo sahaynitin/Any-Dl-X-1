da982a34e9fb3999fac61a9571a256136fdc5d54
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-07 13:23:00 +0530

    Update cb_settings.py

--- plugins/cb_settings.py
+++ plugins/cb_settings.py
@@ -1,7 +1,7 @@
 import pyrogram
-from pyrogram import Client, filters, Message
+from pyrogram import Client, filters
 from plugins.settings import OpenSettings
-
+from pyrogram.types import Message
 @Client.on_message(filters.private & filters.command("settings"))
 async def settings_handler(bot: Client, m: Message):
     editable = await m.reply_text("Please Wait ...", quote=True)

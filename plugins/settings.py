284f61c78402161bd9bace55993ed5cc48188a66
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-07 12:08:25 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -4,32 +4,32 @@ import asyncio
 from pyrogram import types, errors
 from sample_config import Config
 from database.database import db
-
+from pyrogram.types import InlineKeyboardButton
 @Client.on_message(filters.private & filters.command("settings"))
-async def start(m: "types.Message"):
+async def start(bot, update):
     upload_as_doc = user_data.get("upload_as_doc", False)
     caption = user_data.get("caption", None)
     apply_caption = user_data.get("apply_caption", True)
     thumbnail = user_data.get("thumbnail", None)
     buttons_markup = [
-        [types.InlineKeyboardButton(f"Upload as Doc {'✅' if upload_as_doc else '❌'}",
+        [InlineKeyboardButton(f"Upload as Doc {'✅' if upload_as_doc else '❌'}",
                                     callback_data="triggerUploadMode")],
-        [types.InlineKeyboardButton(f"Apply Caption {'✅' if apply_caption else '❌'}",
+        [InlineKeyboardButton(f"Apply Caption {'✅' if apply_caption else '❌'}",
                                     callback_data="triggerApplyCaption")],
-        [types.InlineKeyboardButton(f"Apply Default Caption {'❌' if caption else '✅'}",
+        [InlineKeyboardButton(f"Apply Default Caption {'❌' if caption else '✅'}",
                                     callback_data="triggerApplyDefaultCaption")],
-        [types.InlineKeyboardButton("Set Custom Caption",
+        [InlineKeyboardButton("Set Custom Caption",
                                     callback_data="setCustomCaption")],
-        [types.InlineKeyboardButton(f"{'Change' if thumbnail else 'Set'} Thumbnail",
+        [InlineKeyboardButton(f"{'Change' if thumbnail else 'Set'} Thumbnail",
                                     callback_data="setThumbnail")]
     ]
     if thumbnail:
-        buttons_markup.append([types.InlineKeyboardButton("Show Thumbnail",
+        buttons_markup.append([InlineKeyboardButton("Show Thumbnail",
                                                           callback_data="showThumbnail")])
     if caption:
-        buttons_markup.append([types.InlineKeyboardButton("Show Caption",
+        buttons_markup.append([InlineKeyboardButton("Show Caption",
                                                           callback_data="showCaption")])
-    buttons_markup.append([types.InlineKeyboardButton("Close Message",
+    buttons_markup.append([InlineKeyboardButton("Close Message",
                                                       callback_data="closeMessage")])
 
     try:

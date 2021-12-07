c144f285dbc167267d0a06beb9dd39b0ccdea974
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-07 13:55:23 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -11,9 +11,6 @@ async def OpenSettings(m: Message, user_id: int):
             text="Here You Can Change or Configure Your Settings:",
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton(f"Upload as {'Video' if (get_upload_as_doc(id=user_id)) is False else 'Document'} ✅", callback_data="triggerUploadMode")],
-                    [InlineKeyboardButton(f"Generate Sample Video {'✅' if (get_generate_sample_video(id=user_id)) is True else '❌'}", callback_data="triggerGenSample")],
-                    [InlineKeyboardButton(f"Generate Screenshots {'✅' if (get_generate_ss(id=user_id)) is True else '❌'}", callback_data="triggerGenSS")],
                     [InlineKeyboardButton("Show Thumbnail", callback_data="showThumbnail")],
                     [InlineKeyboardButton("Show Queue Files", callback_data="showQueueFiles")],
                     [InlineKeyboardButton("Close", callback_data="closeMeh")]
